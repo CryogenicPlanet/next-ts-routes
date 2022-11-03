cc6e4b60c76100deeb2893b2b7b1005b50997782
Rahul Tarak <cryogenicplanet@gmail.com>
2022-11-04 05:28:45 +0530

    🥅 Error handling

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+import { RouteError } from './error'
 import { route } from './route'
 
 type Route = Parameters<typeof route>[1]
@@ -19,22 +20,43 @@ export const handler = async <T extends Route>(
       res.status(405).end()
       return
     }
-    const result = await route.GET({ input: req.query }, { req, res, ctx })
-    res.status(200).json(result)
+    try {
+      const result = await route.GET({ input: req.query }, { req, res, ctx })
+      res.status(200).json(result)
+    } catch (err) {
+      if (err instanceof RouteError) {
+        res.status(err.status).json({ message: err.message })
+      }
+      throw err
+    }
   } else if (req.method === 'POST') {
     if (!route.POST) {
       res.status(405).end()
       return
     }
-    const result = await route.POST({ input: req.body }, { req, res, ctx })
-    res.status(200).json(result)
+    try {
+      const result = await route.POST({ input: req.body }, { req, res, ctx })
+      res.status(200).json(result)
+    } catch (err) {
+      if (err instanceof RouteError) {
+        res.status(err.status).json({ message: err.message })
+      }
+      throw err
+    }
   } else if (req.method === 'DELETE') {
     if (!route.DELETE) {
       res.status(405).end()
       return
     }
-    const result = await route.DELETE({ input: req.body }, { req, res, ctx })
-    res.status(200).json(result)
+    try {
+      const result = await route.DELETE({ input: req.body }, { req, res, ctx })
+      res.status(200).json(result)
+    } catch (err) {
+      if (err instanceof RouteError) {
+        res.status(err.status).json({ message: err.message })
+      }
+      throw err
+    }
   } else {
     res.status(405).json({ error: 'Method not found' })
   }
